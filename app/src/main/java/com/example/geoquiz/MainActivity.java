e5fbc8f2725fa8638979ec81aee2921c0feda796
emma <etarazg@gmail.com>
2022-02-13 11:49:18 +0100

    question with answers assigned

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,20 +13,27 @@ public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private void setButtons(boolean answer) {
+
+        Toast correctToast = Toast.makeText(MainActivity.this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT);
+        correctToast.setGravity(Gravity.TOP, 0, 500);
+
+        Toast incorrectToast = Toast.makeText(MainActivity.this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT);
+        incorrectToast.setGravity(Gravity.TOP, 0, 500);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this,
-                    R.string.correct_toast,
-                    Toast.LENGTH_SHORT);
-                    toast.setGravity(Gravity.TOP, 0, 0);
-                    toast.show();
+                if (answer) {
+                    correctToast.show();
+                } else {
+                    incorrectToast.show();
+                }
             }
         });
 
@@ -33,13 +41,38 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+                if (answer) {
+                    incorrectToast.show();
+                } else {
+                    correctToast.show();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        TextView textview = (TextView) findViewById(R.id.questionToShow);
+        textview.setText(R.string.question_text1);
+        setButtons(true);
+
+        textview.setText(R.string.question_text2);
+        setButtons(true);
+
+        textview.setText(R.string.question_text3);
+        setButtons(false);
+
+        textview.setText(R.string.question_text4);
+        setButtons(true);
+
+        textview.setText(R.string.question_text5);
+        setButtons(false);
+
+        textview.setText(R.string.finish_game);
 
     }
 }
