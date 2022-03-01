52d740255ca4e23b05dee075ccdb6822942d48b4
superfer2002 <fporcel2002@gmail.com>
2022-03-01 18:32:23 +0100

    metodos onResume, destroy, etc.

--- app/src/main/java/com/example/geoquiz/controller/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/controller/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.geoquiz.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private int questionNumber = 0;
     private int correctAnswers = 0;
     private ArrayList<Question> questionArrayList = new ArrayList<>();
+
+    private int mCurrentIndex = 0;
+
+    private static final String TAG = "LogLifeCycleCallbacks";
+    private static final String KEY_INDEX = "index";
 
     private void setQuestions () {
 
@@ -53,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
             textview.setText(questionArrayList.get(questionNumber).getQuestionId());
             setButtons(questionArrayList.get(questionNumber).isAnswer());
             previousQuestion.setVisibility(View.GONE);
+            questionNumber = 0;
 
         }
     }
@@ -153,6 +160,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        mCurrentIndex = 0;
 
         setQuestions();
         setContentView(R.layout.activity_main);
@@ -160,4 +169,43 @@ public class MainActivity extends AppCompatActivity {
         moveQuestion();
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState() called");
+        savedInstanceState.putInt("EMMITA", 1);
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 }
